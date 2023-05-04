704a26b5bc5ec15afbb30f6cca21980a09c97678
nkpanda97 <nkpanda97@gmail.com>
2023-05-04 09:54:46 +0200

    1

---
+++ module_2/module_2_file_1.py
@@ -0,0 +1,19 @@
+"""This is a module docstring. This is the module 2 and file 1 of the package.
+"""
+
+def module_2_file_1_func_1(x):
+    """This is a function docstring. This is the module 2 and file 1 of the package.
+    param x: int
+    type x: int
+    return: int
+    """
+    return x + 1
+
+
+def module_2_file_1_func_2(x):
+    """This is a function docstring. This is the module 2 and file 1 of the package.
+    param x: int
+    type x: int
+    return: int
+    """
+    return x + 1
